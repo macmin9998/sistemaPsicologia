0f3f8e345cd46d54ff69ea5e3bebcb444c949697
carlos117 <jose-ccr93@hotmail.com>
2017-06-19 10:29:35 -0500

    paginas usuario con estilos y menu , agregar1 modificado

--- modificar_usuario.php
+++ modificar_usuario.php
@@ -1,13 +1,4 @@
-<?php
-session_start();
-if(isset($_SESSION['sun'])){
 
-
-	
-
-	include("includs/conexion.php");
-
-?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -19,38 +10,84 @@ if(isset($_SESSION['sun'])){
 		<link rel="stylesheet" href="css/font-awesome.min.css">
 		<link rel="stylesheet" href="css/estilos_usuarios.css">
 
-		<script src="https://code.jquery.com/jquery-3.2.1.js" ></script>
-		<script src="javascript/main.js" ></script>
-		<script src="javascript/validaModificarUsuario.js" ></script>
+		<script type="text/javascript" src="js/jquery-3.2.1.js"></script>
+		<script src="js/main.js" ></script>
 
+		 <!--LIGA PARA JQUERY -->
+		
+         
+        <script type="text/javascript">
+					$(document).ready(function(){
+							
+								$('#nuevoUsuario').hide(); 
+								$('#claveN').hide(); 
+								$('#botonModificar').hide();
+								
+
+							$("#continuarB").on( "click", function() {
+								$('#usuario').show(); 
+								$('#claveA').show(); 
+								$('#continuarB').hide();
+								$('#nuevoUsuario').show(); 
+								$('#claveN').show(); 
+								$('#botonModificar').show();
+								
+							 });
+							
+
+					});
+
+			</script>
 
 	</head>
 	<body>
 
+
 		<?php
-		include("menu_pagina.html");
+			include("menu_pagina.html");
 		?>
-		<form action="" method="post" onsubmit="return validar()">
+
+		<form >
+
 			<h2>Modificar Usuario</h2>
-			<input type="text" name="usuario" id="usuario" placeholder="Usuario a modificar">
-			<input type="text" name="nuevo_usuario" id="nuevoUsuario" placeholder="Nuevo Usuario">
-			<input type="password" name="nueva_clave" id="clave" placeholder="Nueva Clave">
-			<input type="submit" value="Modificar Usuario" id="boton" name="modificar">
+			<input type="text"  id="usuario" placeholder="Usuario a modificar">
+			<input type="password"  id="claveA" placeholder="clave Antigua">
+			<input type="button" id="continuarB" value="Continuar">
+
+			<input type="text" id="nuevoUsuario" placeholder="Nuevo Usuario">
+			<input type="password"  id="claveN" placeholder="Nueva Clave">
+			<input type="button" value="Modificar Usuario" id="botonModificar" >
+		
 		</form>
 		<script type="text/javascript">
 			$(function(){
-				$("#boton").click(function(){
+				$("#botonModificar").click(function(){
+
 					var cajaUsuario = $("#usuario").val();
+					var cajaClaveA = $("#claveA").val();
 					var cajaNuevoUsuario = $("#nuevoUsuario").val();
-					var cajaClave =$("#clave").val();
-				$.post('#',
+					var cajaClave =$("#claveN").val();
+				    alert("modificando...");
+
+				$.post('ws/wsUsuarios.php',
 				{
-					WS:"#",
-					usurario:cajaUsuario,
+					WS:"modUsuario",
+					usuarioA:cajaUsuario,
+					claveA : cajaClaveA,
 					nuevoUsuario:cajaNuevoUsuario,
 					clave:cajaClave
+
 					},function(Respuesta){
-						window.location= window.location;
+
+						                alert(Respuesta.Mensaje);
+										if(Respuesta.codMensaje == 100)
+								 	    	window.location =window.location; 
+										else if(Respuesta.codMensaje == 200){
+											
+										    alert(Respuesta.Datos);	
+								        }
+						
+
 				},"json");
 			});
 		});
